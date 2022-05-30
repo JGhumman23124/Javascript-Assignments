a79274840796aee574eeb856697a89e242fe570c
J <jghumman2008@gmail.com>
2022-05-29 17:46:07 -0700

    first commit

---
+++ story_time.js
@@ -0,0 +1,75 @@
+//NOTE: My console.logs are formatted oddly because of the extension prettier.
+
+let name = "Jaiden";
+let location = "San Francisco, California";
+let name_of_mysterious_man = "Merlin";
+let status = "asleep";
+
+let age = 13;
+let lifespan = 10000;
+let year = 1900;
+
+let magical_items = ["Magic Staff", "Magic Sword", "Pool of life"];
+
+let is_a_demigod = true;
+
+console.log(
+  "Prologue: My name is " +
+    name +
+    " and I am " +
+    age +
+    " years old. This story takes place in the year " +
+    year +
+    ". Truthfully, I would have never expected the events that are soon to take place. "
+);
+
+console.log(
+  "I was " +
+    status +
+    " one Saturday afternoon, recovering from my lack of sleep during the week, when I was awoken by a man's voice inside my head. "
+);
+
+console.log(
+  "The man announced that his name was " +
+    name_of_mysterious_man +
+    ". At first I was stunned, but as he began to explain himself, I started to understand his purpose. "
+);
+console.log(
+  name_of_mysterious_man +
+    " wanted to reach the " +
+    magical_items[2] +
+    " in " +
+    location +
+    " to restore his physical body that was reduced to a wisp of a soul in a fight."
+);
+console.log(
+  "He told me that one must be a demigod in order to reach the " +
+    magical_items[2] +
+    ". It took me only moments to realize that the fact I was a demigod was " +
+    is_a_demigod +
+    ". "
+);
+console.log(
+  "He told me to go to " +
+    location +
+    " and find the " +
+    magical_items[2] +
+    ". He gave me a " +
+    magical_items[0] +
+    " and a " +
+    magical_items[1] +
+    " to defend myself against the gaurdians of the " +
+    magical_items[2] +
+    ". "
+);
+console.log(
+  "I arrived at the " +
+    magical_items[2] +
+    " and swiftly defeated the gaurdians. I took a sip of the pool and my lifespan increased to " +
+    lifespan +
+    " years. Also, " +
+    name_of_mysterious_man +
+    " was able to restore his physical form. "
+);
+
+//NOTE: My console.logs are formatted oddly because of the extension prettier.
